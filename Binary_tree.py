68d74a03dce30fc73b802829eb12ddd0bdcc63bc
Ruben Espinoza Osses <r.espinoza05@ufromail.cl>
2021-05-17 00:38:51 -0400

    Commit inicial

---
+++ Binary_tree.py
@@ -0,0 +1,45 @@
+class Node:
+
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+    def print_node(self):
+        print(self.data)
+
+    def insert(self,data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+
+    def print_tree(self):
+        if self.left:
+            self.left.print_tree()
+        print( self.data),
+        if self.right:
+            self.right.print_tree()
+
+
+root = Node(10)
+
+root.insert(9)
+root.insert(9)
+root.insert(9)
+root.insert(9)
+root.insert(9)
+root.insert(9)
+
+
+
+root.print_tree()
---
+++ Binary_tree_mine.py
@@ -0,0 +1,73 @@
+class Node:
+
+    def __init__(self, value=0):
+        self.value = value
+        self.right = None
+        self.left = None
+    
+    def print_node(self):
+        #print ("Current Node: %s, Left Child %s, Right Child %s" % (self.value, self.left.value if (self.left is not None) else "W/O Left Child", self.right.value if (self.right is not None) else "W/O Right Child"), end="\t")
+        print (self.value)
+
+class Tree:
+
+    def __init__(self, root=Node()):
+        self.root = root
+    
+    def insert_node(self,node_to_insert,actual_node=None):
+        if (actual_node is None):
+            actual_node = self.root
+
+        if (node_to_insert.value < actual_node.value):
+            if (actual_node.left is not None):
+                self.insert_node(node_to_insert, actual_node.left)
+            else:
+                actual_node.left = node_to_insert
+        elif (node_to_insert.value > actual_node.value):
+            if (actual_node.right is not None):
+                self.insert_node(node_to_insert, actual_node.right)
+            else:
+                actual_node.right = node_to_insert
+    
+    
+    def diplay_tree(self, current_node=None):
+        
+        if (current_node is None):
+            current_node = self.root
+        current_node.print_node()
+        if (current_node.right is not None):
+            print("\\")
+            self.diplay_tree(current_node.right)
+        if (current_node.left is not None):
+            print("/")
+            self.diplay_tree(current_node.left)
+        
+        
+        
+
+
+tree = Tree()
+tree.insert_node(Node(10))
+tree.insert_node(Node(-10))
+tree.insert_node(Node(-5))
+tree.insert_node(Node(-2))
+tree.insert_node(Node(13))
+tree.insert_node(Node(1296))
+tree.insert_node(Node(13))
+tree.insert_node(Node(123))
+tree.insert_node(Node(0))
+tree.insert_node(Node(1))
+
+
+tree.diplay_tree()
+
+
+        
+
+
+
+
+
+
+
+
